555fbb1c800734daf4d2404214c5867edc7fb959
Przemek <przemek2940@gmail.com>
2021-11-26 15:28:40 +0100

    Trzy liczby, która najmniejsza. Skończone

---
+++ trzyliczby.py
@@ -0,0 +1,28 @@
+#! /home/przemek2940/Python/bin python3
+# -*- coding: utf-8 -*-
+
+op = "t"
+while op == "t":
+    try:
+        a, b, c = input("Podaj trzy liczby oddzielone spacjami: ").split(" ")
+    except ValueError:
+        print("Błąd. Spróbuj jeszcze raz.")
+        continue
+    print("Wprowadzono liczby:", a, b, c)
+    print("\nNajmniejsza: ")
+
+    if a < b:
+        if a < c:
+            najmniejsza = a
+        else:
+            najmniejsza = c
+    elif b < c:
+        najmniejsza = b
+    else:
+        najmniejsza = c
+
+    print(najmniejsza)
+
+    op = input("Jeszcze raz? t/n")
+
+    print("Koniec")
