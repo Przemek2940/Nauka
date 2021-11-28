543f7a94cc37bb7a906a610e4b59637376b594a8
Przemek <przemek2940@gmail.com>
2021-11-28 13:38:48 +0100

    alfabet - wersja z wyborem liter. Skończone

---
+++ alfabet.py
@@ -0,0 +1,24 @@
+#! /home/przemek2940/Python/bin python3
+# -*- coding: utf-8 -*-
+
+n = int(input("Podaj co którą literę chcesz ujrzeć: "))
+print("Alfabet w porządku naturalnym:")
+x = 0
+for i in range(65, 91, n):
+    litera = chr(i)
+    x += 1
+    tmp = litera + " => " + litera.lower()
+    if x == 5:
+        x = 0
+        print("\n", end=" ")
+    print(tmp, end=" ")
+
+x = -1
+print("\nAlfabet w porządku odwróconym:")
+for i in range(122, 96, -n):
+    litera = chr(i)
+    x += 1
+    if x == 5:
+        x = 0
+        print("\n", end=" ")
+    print(litera.upper(), "=>", litera, end=" ")
