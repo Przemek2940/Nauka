2b83701c1ed9fd25c315733bcf6d67f4182b1117
Przemek <przemek2940@gmail.com>
2022-02-11 14:35:16 +0100

    combobox spinbox

--- Qt5/widgets/gui.py
+++ Qt5/widgets/gui.py
@@ -2,8 +2,10 @@
 
 from __future__ import unicode_literals
 from ksztalt import Ksztalty, Ksztalt
-from PyQt5.QtWidgets import QHBoxLayout
-from PyQt5.QtWidgets import QCheckBox, QButtonGroup, QVBoxLayout
+from PyQt5.QtWidgets import QHBoxLayout, QCheckBox, QButtonGroup, QVBoxLayout, QSlider, QLCDNumber, QSplitter
+from PyQt5.QtWidgets import QRadioButton, QGroupBox
+from PyQt5.QtCore import Qt
+
 
 class Ui_Widget(object):
 
@@ -12,6 +14,7 @@ class Ui_Widget(object):
         self.ksztalt2 = Ksztalt(self, Ksztalty.Ellipse)
         self.ksztaltAktywny = self.ksztalt1
 
+        #przyciski
         uklad = QVBoxLayout()
         self.grupaChk = QButtonGroup()
         for i, v in enumerate(('Kwadrat', 'Koło', 'Trójkąt', 'Linia')):
@@ -28,5 +31,35 @@ class Ui_Widget(object):
         ukladH1.addLayout(uklad)
         ukladH1.addWidget(self.ksztalt2)
 
-        self.setLayout(ukladH1)
+        #slider, lcdnumber
+        self.suwak = QSlider(Qt.Horizontal)
+        self.suwak.setMinimum(0)
+        self.suwak.setMaximum(255)
+        self.lcd = QLCDNumber()
+        self.lcd.setSegmentStyle(QLCDNumber.Flat)
+        ukladH2 = QSplitter(Qt.Horizontal, self)
+        ukladH2.addWidget(self.suwak)
+        ukladH2.addWidget(self.lcd)
+        ukladH2.setSizes((125,75))
+
+        #RadioButton
+        self.ukladR = QHBoxLayout()
+        for v in ('R', 'G', 'B'):
+            self.radio = QRadioButton(v)
+            self.ukladR.addWidget(self.radio)
+        self.ukladR.itemAt(0).widget().setChecked(True)
+
+        self.grupaRBtn = QGroupBox('Opcje RGB')
+        self.grupaRBtn.setLayout(self.ukladR)
+        self.grupaRBtn.setObjectName('Radio')
+        self.grupaRBtn.setCheckable(True)
+        ukladH3 = QHBoxLayout()
+        ukladH3.addWidget(self.grupaRBtn)
+
+        ukladOkna = QVBoxLayout()
+        ukladOkna.addLayout(ukladH1)
+        ukladOkna.addWidget(ukladH2)
+        ukladOkna.addLayout(ukladH3)
+
+        self.setLayout(ukladOkna)
         self.setWindowTitle('Widżety')
--- Qt5/widgets/widzety.py
+++ Qt5/widgets/widzety.py
@@ -4,15 +4,41 @@
 from __future__ import unicode_literals
 from PyQt5.QtWidgets import QApplication, QWidget
 from gui import Ui_Widget
+from PyQt5.QtGui import QColor
 
 class Widgety(QWidget, Ui_Widget):
 
+    kanaly = {'R'}
+    kolorW = QColor(0, 0, 0)
     def __init__(self, parent=None):
         super(Widgety, self).__init__(parent)
         self.setupUi(self)
 
         self.grupaChk.buttonClicked[int].connect(self.ustawKsztalt)
         self.ksztaltChk.clicked.connect(self.aktywujKsztalt)
+        for i in range(self.ukladR.count()):
+            self.ukladR.itemAt(i).widget().toggled.connect(self.ustawKanalRbtn)
+        self.suwak.valueChanged.connect(self.zmienKolor)
+
+    def ustawKanalRbtn(self, wartosc):
+        self.kanaly = set()
+        nadawca = self.sender()
+        if wartosc:
+            self.kanaly.add(nadawca.text())
+
+    def zmienKolor(self, wartosc):
+        self.lcd.display(wartosc)
+        if 'R' in self.kanaly:
+            self.kolorW.setRed(wartosc)
+        if 'G' in self.kanaly:
+            self.kolorW.setGreen(wartosc)
+        if 'B' in self.kanaly:
+            self.kolorW.setBlue(wartosc)
+
+        self.ksztaltAktywny.ustawKolorW(
+            self.kolorW.red(),
+            self.kolorW.green(),
+            self.kolorW.blue())
 
     def ustawKsztalt(self, wartosc):
         self.ksztaltAktywny.ustawKsztalt(wartosc)
