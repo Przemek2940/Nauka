59c9346705149617f73af2b32dfa4894a9517bf0
Przemek <przemek2940@gmail.com>
2022-02-16 15:52:52 +0100

    Fizzbuzz pętla for

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+#! /home/przemek2940/PycharmProjects/venv00/bin python3
+# -*- coding: utf-8 -*-
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
